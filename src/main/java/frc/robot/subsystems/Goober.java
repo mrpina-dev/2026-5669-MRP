144e2efed84c5897193355bc5a8abcff38e0649e
aperson78 <jhonenrh@gmail.com>
2026-02-23 14:04:19 -0800

    default turret mode is brake

--- src/main/java/frc/robot/subsystems/Goober.java
+++ src/main/java/frc/robot/subsystems/Goober.java
@@ -1,6 +1,7 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.DutyCycleOut;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -19,6 +20,7 @@ public class Goober extends SubsystemBase {
     configs.SoftwareLimitSwitch.ForwardSoftLimitThreshold = 0.0; //20.0
     configs.SoftwareLimitSwitch.ForwardSoftLimitEnable = true;
 */
+    configs.MotorOutput.NeutralMode = NeutralModeValue.Brake;
     
     motor.getConfigurator().apply(configs);
     }
